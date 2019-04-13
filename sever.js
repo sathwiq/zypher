2d604ac4018a8d04531e99c7e672a5074d2644cb
sathwiq <sathwiq301198@gmail.com>
2019-04-13 10:16:24 +0530

    a

--- sever.js
+++ sever.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.use("/home", homeRoutes);
-app.use("/user", userRoutes);
+app.use("/user", userRoutes);     
 app.listen(process.env.PORT || "3000",function(){
     console.log("its working");
 })
